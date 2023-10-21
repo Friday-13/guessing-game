81668602ad7a0b1ccefb90abedd179b76e49d900
SamsonDm <samsondm97@gmail.com>
2023-10-21 19:55:58 +0300

    feat: task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 1;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    return this.min + Math.ceil((this.max - this.min) / 2);
+  }
 
-    guess() {
+  lower() {
+    this.max -= Math.floor((this.max - this.min) / 2);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min += Math.ceil((this.max - this.min) / 2);
+  }
 }
 
 module.exports = GuessingGame;
